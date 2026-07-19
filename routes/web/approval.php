91a9f8de7e952dd646c48f61857665356a9fa480
Ongkiboy <rizqi30syahrulm@gmail.com>
2026-07-19 23:40:26 +0700

    fix(route): memperbaiki metode HTTP

--- routes/web/approval.php
+++ routes/web/approval.php
@@ -18,9 +18,9 @@ Route::middleware(['auth', 'cek.admin'])->group(function () {
 // =============================================
 Route::middleware(['auth', 'cek.admin'])->prefix('admin/approval')->name('admin.approval.')->group(function () {
 
-    Route::get('/',                         [ApprovalController::class, 'indeks']) ->name('indeks');
-    Route::post('/{peminjaman}/setujui',    [ApprovalController::class, 'setujui'])->name('setujui');
-    Route::post('/{peminjaman}/tolak',      [ApprovalController::class, 'tolak'])  ->name('tolak');
-    Route::post('/{peminjaman}/diambil',    [ApprovalController::class, 'diambil'])->name('diambil');
-    Route::post('/{peminjaman}/kembali',    [ApprovalController::class, 'kembali'])->name('kembali');
+    Route::get('/',                          [ApprovalController::class, 'indeks']) ->name('indeks');
+    Route::patch('/{peminjaman}/setujui',    [ApprovalController::class, 'setujui'])->name('setujui');
+    Route::patch('/{peminjaman}/tolak',      [ApprovalController::class, 'tolak'])  ->name('tolak');
+    Route::patch('/{peminjaman}/diambil',    [ApprovalController::class, 'diambil'])->name('diambil');
+    Route::patch('/{peminjaman}/kembali',    [ApprovalController::class, 'kembali'])->name('kembali');
 });

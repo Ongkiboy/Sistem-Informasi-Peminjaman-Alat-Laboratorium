af374a2eac6d67de14c381aff0af44802891124f
Ongkiboy <rizqi30syahrulm@gmail.com>
2026-07-19 23:51:53 +0700

    fix(Auth): memperbaiki kesalahan penulisan dan mendaftarkan Auth

--- app/Http/Middleware/cekAdmin.php
+++ app/Http/Middleware/cekAdmin.php
@@ -16,7 +16,7 @@ class cekAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!Auth::check() || !Auth::user()->is_admin()){
+        if (!Auth::check() || !Auth::user()->isAdmin()){
             abort(403, 'Akses ditolak. Halaman ini hanya dapat diakses oleh admin.');
         } 
     
